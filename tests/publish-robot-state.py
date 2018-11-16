6be905af084dc5da58cd4fdc34d64d9debe1213d
Olivier Stasse <ostasse@laas.fr>
2018-11-16 17:47:55 +0100

    Add files to perform servo-off.

---
+++ tests/publish-robot-state.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+import sys
+import rospy
+
+from std_srvs.srv import *
+from dynamic_graph_bridge.srv import *
+from dynamic_graph_bridge_msgs.srv import *
+
+def launchScript(code,title,description = ""):
+    raw_input(title+':   '+description)
+    rospy.loginfo(title)
+    rospy.loginfo(code)
+    for line in code:
+        if line != '' and line[0] != '#':
+            print line
+            answer = runCommandClient(str(line))
+            rospy.logdebug(answer)
+            print answer
+    rospy.loginfo("...done with "+title)
+
+
+# Waiting for services
+try:
+    rospy.loginfo("Waiting for run_command")
+    rospy.wait_for_service('/run_command')
+    rospy.loginfo("...ok")
+
+    rospy.loginfo("Waiting for start_dynamic_graph")
+    rospy.wait_for_service('/start_dynamic_graph')
+    rospy.loginfo("...ok")
+
+    runCommandClient = rospy.ServiceProxy('run_command', RunCommand)
+    runCommandStartDynamicGraph = rospy.ServiceProxy('start_dynamic_graph', Empty)
+
+    initCode = open( "servooff.py", "r").read().split("\n")
+    
+    rospy.loginfo("Stack of Tasks launched")
+
+
+    launchScript(initCode,'Publishing robotState_ros')
+
+except rospy.ServiceException, e:
+    rospy.logerr("Service call failed: %s" % e)
+
